d442ecdf450513a5fa39837cd0dd3edbefb70104
Gildas Montcho <gmontcho@gmail.com>
2024-10-18 12:26:34 +0200

    modifier TestAvion en utilisant les constructeurs

--- src/fr/diginamic/instances/entites/TestAvion.java
+++ src/fr/diginamic/instances/entites/TestAvion.java
@@ -5,50 +5,23 @@ import java.time.Month;
 
 public class TestAvion {
   public static void main(String[] args) {
-    Avion avion1 = new Avion();
-    Avion avion2 = new Avion();
+    Avion avion1 = new Avion("AVION-01-BOEING", "Boeing", "747");
+    Avion avion2 = new Avion("AVION-02-AIRBUS", "Airbus", "A340");
 
-    Passager passager1 = new Passager();
-    passager1.setNom("Cali");
-    passager1.setPrenom("Justine");
-    passager1.setNumeroSiege("A1");
+    Passager passager1 = new Passager("Cali", "Justine", "A1");
+    Passager passager2 = new Passager("Verdeau", "Luc", "A2");
+    Passager passager3 = new Passager("Anglin", "Jacques", "A3");
+    Passager passager4 = new Passager("Limon", "Frédérique", "A4");
 
-    Passager passager2 = new Passager();
-    passager2.setNom("Verdeau");
-    passager2.setPrenom("Luc");
-    passager2.setNumeroSiege("A2");
-
-    Passager passager3 = new Passager();
-    passager3.setNom("Anglin");
-    passager3.setPrenom("Jacques");
-    passager3.setNumeroSiege("A3");
-
-    Passager passager4 = new Passager();
-    passager4.setNom("Limon");
-    passager4.setPrenom("Frédérique");
-    passager4.setNumeroSiege("A4");
-
-    Pilote pilote1 = new Pilote();
-    pilote1.setNom("Jean");
-    pilote1.setPrenom("Dupont");
-    pilote1.setDateLicence(LocalDate.of(2012, Month.OCTOBER, 17));
+    Pilote pilote1 = new Pilote("Dupont", "Jean", LocalDate.of(2012, Month.OCTOBER, 17));
+    Pilote pilote2 = new Pilote("Bateau", "Alice", LocalDate.of(2014, Month.FEBRUARY, 4));
     //      System.out.println(pilote1);
+    //      System.out.println(pilote2);
 
-    Pilote pilote2 = new Pilote();
-    pilote2.setNom("Alice");
-    pilote2.setPrenom("Bateau");
-    //      pilote2.setDateLicence(LocalDate.of(2014, Month.FEBRUARY, 4));
-
-    avion1.setMarque("Boeing");
-    avion1.setModele("747");
-    avion1.setImmatriculation("AVION-01-BOEING");
     avion1.setPilote(pilote1);
     avion1.setPassagers(new Passager[] {passager1, passager2});
     System.out.println(avion1);
 
-    avion2.setMarque("Airbus");
-    avion2.setModele("A340");
-    avion2.setImmatriculation("AVION-02-AIRBUS");
     avion2.setPilote(pilote2);
     avion2.setPassagers(new Passager[] {passager3, passager4});
     System.out.println(avion2);
